1fb0d7ad06f3a7109c99057e82459ef47e255b41
soohwan97 <qkqhzz120@naver.com>
2022-04-09 02:15:35 +0900

    solved / charAt(), string.equals(), Integer.toString(), string.length()-1

--- src/Baekjun_1110.java
+++ src/Baekjun_1110.java
@@ -4,29 +4,32 @@ public class Baekjun_1110 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		String n = sc.next();
-		String p = n;
-		final int c = 48;	
-		if(n.length()==1) {
-			n += 0;
-		}
-		
-		int num1 = n.charAt(0)-c;
-		int num2 = n.charAt(1)-c;
-		int sum = num1 + num2;
-		String a = Integer.toString(sum);
-		String s = Integer.toString(num2) + a.charAt(a.length()-1);
-		int i=1;
-		boolean plz = true;
-		while (plz) {
-			int num3 = s.charAt(0)-c;
-			int num4 = s.charAt(1)-c;
-			sum = num3 + num4;
-			a = Integer.toString(sum);
-			s = Integer.toString(num4) + a.charAt(a.length()-1);
-			i+=1;
-			if(s == p) plz=false;
-		}
-		System.out.println(i);
+		final int c = 48;
+		if (!n.equals("0")) {
+			if (n.length() == 1) {
+				n += 0;
+			}
+			int num1 = n.charAt(0) - c;
+			int num2 = n.charAt(1) - c;
+			int sum = num1 + num2;
+			String a = Integer.toString(sum); //int -> string
+			String s = Integer.toString(num2) + a.charAt(a.length() - 1); //a.length() - 1 가장 오른쪽 문자
+			int i = 1;
+			if (s != n) {
+				boolean plz = true; //while 탈출, 범위가 정해져있지 않기 때문에 while활용
+				while (plz) {
+					int num3 = s.charAt(0) - c;
+					int num4 = s.charAt(1) - c;
+					sum = num3 + num4;
+					a = Integer.toString(sum);
+					s = Integer.toString(num4) + a.charAt(a.length() - 1);
+					i += 1;
+					if (s.equals(n))
+						plz = false;
+				}
+			}
+			System.out.println(i);
+		} else System.out.println("1");
 		sc.close();
 	}
 }
